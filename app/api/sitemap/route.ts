abc495b31cc5e15f4faa0282b79f8b6618c5f9f2
Rich Man <uriemanda7@gmail.com>
2025-06-27 13:07:31 +0700

    Create route.ts

---
+++ app/api/sitemap/route.ts
@@ -0,0 +1,19 @@
+// /app/api/sitemap/route.ts
+
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://imageresizeronline.vercel.app/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
