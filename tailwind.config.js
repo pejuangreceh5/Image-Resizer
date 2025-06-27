a31c628d4192b546814ad03d1085dd667ce01d70
Rich Man <uriemanda7@gmail.com>
2025-06-27 19:25:12 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // WAJIB: biar nggak otomatis dark
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
